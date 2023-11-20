4a573c5fcca6ffa75f5989c606c8ab316d8aae60
jerrylin1102 <98633563+jerrylin1102@users.noreply.github.com>
2023-11-20 19:40:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ Created on Wed Jun  2 21:16:35 2021
 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 
 Line Bot聊天機器人
-第一章 Line Bot申請與串接
-Line Bot機器人串接與測試
+第四章 選單功能
+按鈕樣板TemplateSendMessage
 """
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
@@ -27,7 +27,7 @@ line_bot_api = LineBotApi('q8MasGUuwFhWx+O1opafyqY9vwRoy7RvYQTjgGcyjNFaI4x063mir
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('7a4ff39e79a9b8961c5ddf5f3b9a8bbf')
 
-line_bot_api.push_message('Uaf6d62add8a5bce9a9a64d1d1d97abd2', TextSendMessage(text='歡迎使用1'))
+line_bot_api.push_message('Uaf6d62add8a5bce9a9a64d1d1d97abd2', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -51,31 +51,34 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    #line_bot_api.reply_message(event.reply_token,message)
-    if re.match('開始使用',message):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
         buttons_template_message = TemplateSendMessage(
         alt_text='這個看不到',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://i.imgur.com/YSJayCb.jpeg',
-            title='記帳機器人',
-            text='選單功能',
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
             actions=[
                 PostbackAction(
-                    label='使用者',
-                    display_text='請輸入姓名:',
+                    label='偷偷傳資料',
+                    display_text='檯面上',
                     data='action=檯面下'
-                    #line_bot_api.push_message('Uaf6d62add8a5bce9a9a64d1d1d97abd2', TextSendMessage(text='歡迎使用'))
                 ),
-                PostbackAction(
-                    label='記帳',
+                MessageAction(
+                    label='光明正大傳資料',
                     text='我就是資料'
                 ),
+                URIAction(
+                    label='行銷搬進大程式',
+                    uri='https://marketingliveincode.com/'
+                )
             ]
         )
     )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
-
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
